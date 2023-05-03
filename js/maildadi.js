9b605e26780943628edbe9cf6091affe10fd88cd
lorenzotroiano <lorenzo.troiano1999@gmail.com>
2023-05-03 14:34:23 +0200

    creata l'arrey e il ciclo

---
+++ js/maildadi.js
@@ -0,0 +1,27 @@
+
+
+
+// Chiedi all’utente la sua email, 
+let inserisciEmail = prompt("Inserisci la tua mail");
+
+const emailRichiesta = ["qwerty@libero.it" , "ytrewq@hotmail.it" , "sium@gmail.com" , "giusepperossi@gmail.com"];
+
+emailRichiesta.push('giorgiorossi@libero.it');
+
+// console.log(emailRichiesta);
+
+
+
+
+// controlla che sia nella lista di chi può accedere
+
+for ( let i = 0; i < emailRichiesta.length; i++) {
+    
+    console.log(emailRichiesta[i]);
+
+
+   
+}
+
+//  stampa un messaggio appropriato sull’esito del controllo.
+
