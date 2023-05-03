940fa5d76d7f96694470e9c94317f7a75bc7814c
lorenzotroiano <lorenzo.troiano1999@gmail.com>
2023-05-03 16:58:21 +0200

    ordinato codice

--- dado/js/dado.js
+++ dado/js/dado.js
@@ -1,35 +1,26 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
-
 // let numero = parseInt(prompt("Inserisci un numero da 1 a 6"));
 const button = document.getElementById('myButton');
 
 button.addEventListener('click' , 
 function () {
 
+    // numero random utente
     let numero = Math.floor((Math.random() * 6) + 1);
     
     let userNumero = "L'user sceglie il numero:" + numero;
 
-
     document.getElementById("user").innerHTML = userNumero;
 
 
-
-
-
-
-
-
+    // numero random pc
     let n = Math.floor((Math.random() * 6) + 1);
 
-let computerN = "Il computer sceglie il numero:"  + n;
+    let computerN = "Il computer sceglie il numero:"  + n;
 
-document.getElementById("pc").innerHTML = computerN;
-
-
-
+    document.getElementById("pc").innerHTML = computerN;
 
 
 if (n > numero) {
