84d2d9cd0aa58237845b08586a058cd6091efaac
lorenzotroiano <lorenzo.troiano1999@gmail.com>
2023-05-03 16:16:24 +0200

    sistemato primo esercizio

--- js/maildadi.js
+++ js/maildadi.js
@@ -1,50 +1,38 @@
-let messaggio;
+let messaggio = "Ciao User, non sei registrato, ti invitiamo a farlo!";
 
 // Chiedi all’utente la sua email, 
 const emailPresente = ["qwerty@libero.it" , "ytrewq@hotmail.it" , "sium@gmail.com" , "giusepperossi@gmail.com"];
 
-emailPresente.push('giorgiorossi@libero.it');
+// emailPresente.push('giorgiorossi@libero.it');
 
 let container = document.getElementById('container');
 
-
 const button = document.getElementById('myButton');
+
+const flag = false;
 
 button.addEventListener('click',
 function() {
-    
+
     const emailInserita = document.querySelector('input').value;
 
-  for ( let i = 0; i < emailPresente.length; i++) {
+    for ( let i = 0; i < emailPresente.length; i++) {
 
+        const valore = emailPresente[i];
 
-    const valore = emailPresente[i];
+        // controlla che sia nella lista di chi può accedere
+        if ( valore === emailInserita) {
 
-    // controlla che sia nella lista di chi può accedere
-    if (valore === emailInserita) {
+            messaggio = "Ciao, bentornato tra noi!"
 
-        messaggio = "Ciao, bentornato tra noi!"
-        
+        }
+
     }
-    else {
-
-        messaggio = "Ciao User, non sei registrato, ti invitiamo a farlo!"
-    
-    }
-
 
     //  stampa un messaggio appropriato sull’esito del controllo.
     const benvenutoMessaggio = document.createElement('h1');
     benvenutoMessaggio.append(messaggio);
     container.append(benvenutoMessaggio);
-
-
-
-  }
-
-
-
-    
 })
 
 
